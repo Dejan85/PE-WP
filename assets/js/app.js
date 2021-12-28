669ca9a266c20c50895697a4131993fef49a8b76
Dejan <dejan.markovic@cinnamon.agency>
2021-12-28 04:14:35 +0100

    refactored app.js file and implemented new logic

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,35 +1,5 @@
-let newSwiper = new Swiper(".new-swiper", {
-    spaceBetween: 24,
-    loop: 'true',
-    slidesPerView: "auto",
-    centeredSlides: true,
+import { dom } from "./dom-logic/index.js";
 
-    pagination: {
-        el: ".swiper-pagination",
-        dynamicBullets: true,
-    },
-    breakpoints: {
-        992: {
-            spaceBetween: 80,
-            slidesPerView: 2,
-        },
-    },
-});
-
-
-let buttonOpen = document.querySelector('#hamburger');
-let buttonClose = document.querySelector('#button-close');
-const nav = document.querySelector('.nav');
-
-if (buttonOpen) {
-    buttonOpen.addEventListener('click', function() {
-        nav.classList.add('is-active');
-    });
-}
-
-
-if (buttonClose) {
-    buttonClose.addEventListener('click', function() {
-        nav.classList.remove('is-active');
-    })
-}
+// RESPONSIVE NAV CLOSE LOGIC
+dom.mobileMenuClickEvent();
+dom.showHeaderShadowOnScroll();
