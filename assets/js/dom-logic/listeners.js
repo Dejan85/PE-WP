33fd936a22ed86d26da77f3d97e66a8078638f73
Dejan <dejan.markovic@cinnamon.agency>
2021-12-28 04:13:11 +0100

    created listeners

---
+++ assets/js/dom-logic/listeners.js
@@ -0,0 +1,17 @@
+export const mobileMenuClickEvent = function () {
+  this.buttonOpen.addEventListener("click", () =>
+    this.nav.classList.add("is-active")
+  );
+
+  this.buttonClose.addEventListener("click", () =>
+    this.nav.classList.remove("is-active")
+  );
+};
+
+export const showHeaderShadowOnScroll = function () {
+  window.addEventListener("wheel", (e) =>
+    e.deltaY === 100
+      ? this.header.classList.add("header-scroll")
+      : this.header.classList.remove("header-scroll")
+  );
+};
